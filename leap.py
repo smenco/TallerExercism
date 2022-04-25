e6e64f9d14fd93742afbeff32aa4ba5dd26bb957
Sergio Rafael Menco Ariza <smenco@serfinansa.local>
2022-04-25 14:13:28 -0500

    decimo commit

---
+++ leap.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 25 14:12:06 2022
+
+@author: smenco
+"""
+
+def leap_year(year):
+    if year % 400 == 0:
+        return True
+    
+    if year % 100 == 0:
+        return False
+    
+    if year % 4 == 0:
+        return True
+    else:
+        return False
