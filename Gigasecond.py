5f647a1480a2822d07c9238a090beb4176844071
Sergio Rafael Menco Ariza <smenco@serfinansa.local>
2022-04-25 09:32:59 -0500

    tercer commit

---
+++ Gigasecond.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 25 09:29:19 2022
+
+@author: smenco
+"""
+
+from datetime import timedelta
+
+gigasecond = timedelta(seconds = 10**9)
+
+def add(date):
+    
+    return date + gigasecond
